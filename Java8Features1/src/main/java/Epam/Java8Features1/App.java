7ce62518f2b6ed6b1cf38e2e913ac275f2ee8d11
hammu116 <59144819+hammu116@users.noreply.github.com>
2020-07-22 12:39:07 +0530

    Add files via upload

---
+++ Java8Features1/src/main/java/Epam/Java8Features1/App.java
@@ -0,0 +1,13 @@
+package Epam.Java8Features1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
